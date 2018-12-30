32a18b4f329c5116795dbbb46250fa8f01c5fff4
zachvalenta <zjv5002@gmail.com>
2018-12-30 16:18:46 -0500

    step 4

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -17,8 +17,27 @@ app.config.update(dict(
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 
+# plug in the light
 def connect_db():
     """Connects to the specific database."""
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
+
+
+# turn the light on
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+# turn the light off
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
